6a71d454f413c29f9aaea0193dd8a283a2d48ff8
Vallis-LFC <115032819+Vallis-LFC@users.noreply.github.com>
2024-03-14 22:54:38 -0400

    Add files via upload

--- main.c
+++ main.c
@@ -8,105 +8,122 @@ FILE* csv;
 
 //function to get the amount of rows
 int get_length(char str[1000]){
-    FILE* input;
-    int count = 0;
-    char buffer[1000];
-    input = fopen(str, "r");
-    while(fgets(buffer,sizeof(buffer),input)){ 
-        count+=1; 
-    }
-    fclose(input);
-    return count-2;
+	FILE* input;
+	int count = 0;
+	char buffer[1000];
+	input = fopen(str, "r");
+	while(fgets(buffer,sizeof(buffer),input)){ 
+		count+=1; 
+	}
+	fclose(input);
+	return count-2;
 }
 
 
 
 int main(){
-    char buffer[1000];
-    char *data;
-    int count = 0;
-    char *dataptr;
-    double converted;
-    int index = 0;
-    
-    //intializes the arrays
-    count = get_length("GlobalTemperatures.csv");
-    char dates[count][20];
-    double landAvg[count];
-    double landAvgUn[count];
-    double landMax[count];
-    double landMaxUn[count];
-    double landMin[count];
-    double landMinUn[count];
-    double land_seaAvg[count];
-    double land_seaAvgUn[count];
+	char buffer[1000];
+	char *data;
+	int count = 0;
+	char *dataptr;
+	double converted;
+	int index = 0;
+	
+	//intializes the arrays
+	count = get_length("GlobalTemperatures.csv");
+	char dates[count][20];
+	double landAvg[count];
+	double landAvgUn[count];
+	double landMax[count];
+	double landMaxUn[count];
+	double landMin[count];
+	double landMinUn[count];
+	double land_seaAvg[count];
+	double land_seaAvgUn[count];
 
 
-    csv = fopen("GlobalTemperatures.csv","r");
-    //checks if the file is valid
-    if(!csv){
-        printf("Error: could not open file");
-        return 0;
-    }
+	csv = fopen("GlobalTemperatures.csv","r");
+	//checks if the file is valid
+	if(!csv){
+		printf("Error: could not open file");
+		return 0;
+	}
 
 //Loops through the first line once to get rid of the labels
-    fgets(buffer,sizeof(buffer),csv);
-    data = strtok(buffer,",");
-    for(int i = 0;i<8;i++){
-        data = strtok(NULL, ",");
-    }
+	fgets(buffer,sizeof(buffer),csv);
+	data = strtok(buffer,",");
+	for(int i = 0;i<8;i++){
+		data = strtok(NULL, ",");
+	}
 
 //gets all the data and sorts them into arrays
-    
-    printf("%d ",count);
+	
    while(fgets(buffer,sizeof(buffer),csv)){    
-        data = strtok(buffer,",");
-        strcpy(dates[index],data); //copys the dates to the array
+		data = strtok(buffer,",");
+		strcpy(dates[index],data); //copys the dates to the array
 
-        //loops through 7 times to get the values
-        for(int i = 0;i<=7;i++){
-            
-            data = strtok(NULL, ","); //uses comma as delimitter and parses through
-            if (data!=NULL){ //checks if value is Null
-                converted = strtod(data, &dataptr); //converts the data to a double 
-                switch (i)  //uses a switch statement to determine which data goes into which array
-                {
-                case 0:
-                    landAvg[index] = converted;
-                    break;
-                case 1:
-                    landAvgUn[index] = converted;
-                    break;
-                case 2:
-                    landMax[index] = converted;
-                    break;
-                case 3:
-                    landMaxUn[index] = converted;
-                    break;
-                case 4:
-                    landMin[index] = converted;
-                    break;
-                case 5:
-                    landMinUn[index] = converted;
-                    break;
-                case 6:
-                    land_seaAvg[index] = converted;
-                    break;
-                case 7:
-                    land_seaAvgUn[index] = converted;
-                    break;
-                default:
-                    printf("something wrong happened");
-                    break;
-                }
+		//loops through 7 times to get the values
+		for(int i = 0;i<=7;i++){
+			
+			data = strtok(NULL, ","); //uses comma as delimitter and parses through
+			if (data!=NULL){ //checks if value is Null
+				converted = strtod(data, &dataptr); //converts the data to a double 
+				switch (i)  //uses a switch statement to determine which data goes into which array
+				{
+				case 0:
+					landAvg[index] = converted;
+					break;
+				case 1:
+					landAvgUn[index] = converted;
+					break;
+				case 2:
+					landMax[index] = converted;
+					break;
+				case 3:
+					landMaxUn[index] = converted;
+					break;
+				case 4:
+					landMin[index] = converted;
+					break;
+				case 5:
+					landMinUn[index] = converted;
+					break;
+				case 6:
+					land_seaAvg[index] = converted;
+					break;
+				case 7:
+					land_seaAvgUn[index] = converted;
+					break;
+				default:
+					printf("something wrong happened");
+					break;
+				}
 
-            }     
-        }
-        index+=1;
+			}     
+		}
+		index+=1;
    }
-    fclose(csv);
-    
-    
+	fclose(csv);
+	
+   //start ur coding here
+	int length = count - 10*12;
+	int years = length/12;
+	int average;
 
-    return 0;
+	double sum = 0.0;   
+	int placeholder = 12;
+	// Loop to add the first 12 numbers
+	for(int i = 0; i < years; i++) {
+		int month_count = ((i*placeholder));
+		for(int j = 0; j < 12; j++) {
+			int k = month_count + j;
+			sum += landAvg[k];
+
+		}
+		average = sum/12;
+		printf("the average is: %d\n", average);
+		sum = 0.0;
+	}
+
+	return 0;
 }
